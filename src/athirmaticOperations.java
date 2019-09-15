4b178e61df38a6c68ee96b72454f3b05a0a26b1a
Sagar Khot <sagar22khot@gmail.com>
2019-09-15 12:20:51 -0400

    Arithmetic Operations
    Basic operations to higher and radian operations

---
+++ src/athirmaticOperations.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class athirmaticOperations {
+    public static void main(String[] args) {
+
+        // Basic Operations
+        System.out.println(5 + 4);
+        System.out.println(5 - 4);
+        System.out.println(5 / 4);
+        System.out.println(5 * 4);
+        System.out.println(5 % 4);
+        System.out.println(5.0 / 4.0);
+        int var1 = 10;
+        System.out.println(var1++);
+        System.out.println(++var1);
+        System.out.println(var1);
+
+        // Higher Operations
+        System.out.println(Math.abs(-1));
+        System.out.println(Math.ceil(4.25));
+        System.out.println(Math.floor(4.25));
+        System.out.println(Math.round(4.25));
+        System.out.println(Math.max(4, 5));
+        System.out.println(Math.min(4, 5));
+        System.out.println(Math.exp(1));
+        System.out.println(Math.log(1));
+        System.out.println(Math.log10(1));
+        System.out.println(Math.pow(2, 2));
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.cbrt(4));
+        System.out.println(Math.hypot(5, 5));
+        System.out.println(Math.PI);
+
+        // Radian Operations
+        System.out.println(Math.sin(1.5708));
+        System.out.println(Math.cos(1.5708));
+        System.out.println(Math.tan(1.5708));
+        System.out.println(Math.asin(1.5708));
+        System.out.println(Math.acos(1.5708));
+        System.out.println(Math.atan(1.5708));
+        System.out.println(Math.sinh(1.5708));
+        System.out.println(Math.cosh(1.5708));
+        System.out.println(Math.tanh(1.5708));
+        System.out.println(Math.toDegrees(90));
+        System.out.println(Math.toRadians(90));
+        // Others include colt, jet, weka, Jscience, mahout, apache commons math
+
+
+    }
+}
